116cb2436fde87576ec00015d4215c7a4bf2e215
jules01 <iulian.pascalau@gmail.com>
2023-06-06 15:38:41 +0300

    - log level trace fix

--- libp2p/netMessenger.go
+++ libp2p/netMessenger.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	logging "github.com/ipfs/go-log"
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	pubsubPb "github.com/libp2p/go-libp2p-pubsub/pb"
